19d5066bc773091f40521411c2afe40194468de1
Damian Krzeminski <pirxpilot@code42day.com>
2017-04-20 10:22:47 -0700

    add in-browser test support

--- Makefile
+++ Makefile
@@ -1,9 +1,31 @@
+PROJECT=tile-cache
+NODE_BIN=./node_modules/.bin
+SRC = index.js $(wildcard lib/*.js)
+
+all: check compile
+
 check: lint test
 
-lint:
-	./node_modules/.bin/jshint *.js lib test
+compile: build/build.js
+
+build:
+	mkdir -p $@
+
+build/build.js: node_modules $(SRC) | build
+	$(NODE_BIN)/browserify --require ./index.js:$(PROJECT) --outfile $@
+
+.DELETE_ON_ERROR: build/build.js
+
+node_modules: package.json
+	yarn && touch $@
+
+lint: | node_modules
+	$(NODE_BIN)/jshint $(SRC) test
 
 test:
-	./node_modules/.bin/mocha --recursive --require should
+	$(NODE_BIN)/electron-mocha --require should --renderer
 
-.PHONY: check lint test
+clean:
+	rm -fr build node_modules
+
+.PHONY: all clean check lint compile test
--- test/tile-cache.js
+++ test/tile-cache.js
@@ -1,9 +1,41 @@
 var should = require('should');
 var tileCache = require('../');
 
-describe('tile-cache node module', function () {
-  it('must have at least one test', function () {
-    tileCache();
-    should.fail('Need to write tests.');
+describe('tile-cache', function () {
+  var tile1 = new Uint8Array([1, 2, 3, 4]).buffer;
+  var tile2 = new Uint8Array([4, 3, 2, 1, 0]).buffer;
+
+  before(function(done) {
+    tileCache.putTile([1, 1, 5], tile1, done);
   });
+
+  it('must retrieved stored tiles', function (done) {
+    tileCache.getTile([1, 1, 5], function(err, tile) {
+      should.not.exist(err);
+      tile.should.have.property('byteLength', 4);
+      tile.should.eql(tile1);
+      done(err);
+    });
+
+  });
+
+  it('must returned empty when not found', function(done) {
+    tileCache.getTile([1, 2, 5], function(err, tile) {
+      should.not.exist(tile);
+      done(err);
+    });
+  });
+
+  it('must store new tiles', function(done) {
+    tileCache.putTile([2, 1, 5], tile2, function(err) {
+      should.not.exist(err);
+      tileCache.getTile([2, 1, 5], function(err, tile) {
+        should.not.exist(err);
+        tile.should.have.property('byteLength', 5);
+        tile.should.eql(tile2);
+        done(err);
+      });
+    });
+  });
+
 });
