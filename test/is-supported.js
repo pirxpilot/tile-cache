6a00a925cb162c9840f1733b607d8a7a44d07ff6
Damian Krzeminski <pirxpilot@furkot.com>
2025-02-05 11:06:06 -0700

    remove callback API - support promised based API only

--- test/is-supported.js
+++ test/is-supported.js
@@ -8,13 +8,6 @@ const { supported } = require('../');
 
 describe('supported', async function () {
 
-  await it('should be supported with callback', function (_, done) {
-    supported(function (err, ok) {
-      ok.should.be.ok();
-      done(err);
-    });
-  });
-
   await it('should be supported with promise', async function () {
     const ok = await supported();
     ok.should.be.ok();
